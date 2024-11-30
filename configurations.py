3faa0a1a084184d9cdfdb2e07114a44534b21731
SebySurfer <sebysurfer@gmail.com>
2024-11-29 19:39:30 -0600

    Database name
    Missed name

--- configurations.py
+++ configurations.py
@@ -3,7 +3,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import certifi
 
-uri = "mongodb+srv://sebysurfer:snowden5889@cluster01.z0tvx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01"
+uri = "mongodb+srv://sebysurfer:snowden5889@cluster01.z0tvx.mongodb.net/users_db?retryWrites=true&w=majority&appName=Cluster01"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
